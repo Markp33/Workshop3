a896616e57fa955ad9004d3c3516e7d0b37ac4da
Markp33 <marrrk700@yahoo.com>
2024-01-12 10:58:23 +0100

    YOUTUBE website

---
+++ commentaarform.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Commentaar toevoegen</h1>
+
+    <form action = "commentaarresultaat.php" method="POST">
+
+<div>
+    naam: <input type="text" name="naam" value="">
+</div>
+<div>
+    email: <input type="text" name="email">
+</div>
+<div>
+    <textarea name = "commentaar" cols="30" rows = "10"></textarea>
+</div>
+
+<input type="submit" value = "verzenden">
+</form>
+
+<video width="400" controls>
+  <source src="assets/movie.mp4" type="video/mp4">
+  <source src="mov_bbb.ogg" type="video/ogg">
+  Your browser does not support HTML video.
+</video>
+
+</body>
+</html>
